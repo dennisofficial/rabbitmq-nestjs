5496aa39aa0e2e1b1bbd637753468378d9c9ef70
dennisofficial <34203433+dennisofficial@users.noreply.github.com>
2023-05-18 23:35:12 -0400

    Revert "Commit major change"

--- api/apps/email/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { EmailModule } from './email.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(EmailModule);
-  await app.listen(3000);
-}
-bootstrap();
--- api/apps/roles/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { RolesModule } from './roles.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(RolesModule);
-  await app.listen(3000);
-}
-bootstrap();
